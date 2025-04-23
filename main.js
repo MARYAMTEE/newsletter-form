53a848454bebf0a2de828325da891bb096c21816
MARYAMTEE <ayorindemaryam078@gmail.com>
2025-04-23 17:16:51 +0100

    update form error and success message

--- main.js
+++ main.js
@@ -1,8 +1,37 @@
-const form = document.getElementById('form')
+const form = document.getElementById('form');
+const errorMessage = document.getElementById('errorMessage');
+const successMessage = document.getElementById('successMessage');
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const formDataEntries = new FormData(newletterForm).entries();
-    const { email } = Object.fromEntries(formDataEntries);
+    const email = form.email.value.trim();
+
+    let errors = [];
+
+    if(!email) {
+        errors.push("Valid email required");
+    }else if (typeof email !== 'string') {
+        errors.push("Valid email required");
+    }else if(email.length < 5 || email.length > 50) {
+        errors.push("Valid email required");
+    }else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+        errors.push("Valid email required");
+      }
+
+    if(errors.length > 0) {
+        errorMessage.textContent = errors[0];
+        errorMessage.className = "errorMessage";
+        errorMessage.style.display = "block";
+        successMessage.classList.add("hidden");
+    } else {
+        errorMessage.textContent= "";
+        errorMessage.style.display = "none";
+        successMessage.classList.remove("hidden");
+    }
 });
+
+function dismissSuccess() {
+    successMessage.classList.add("hidden");
+    form.reset();
+}
